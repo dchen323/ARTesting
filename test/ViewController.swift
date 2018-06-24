b64ab0533b3da18041f14a43f2d203d382bc366f
Daniel <d.chen323@gmail.com>
2018-06-23 20:39:53 -0700

    "Built test code that allows you to place planes on surfaces"

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -11,23 +11,21 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
+    var planeArray = [SCNNode]()
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +33,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        configuration.planeDetection = .horizontal
+        
+        
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -47,34 +49,76 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+                addPlane(atLocation: hitResult)
+            }
+        }
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func addPlane(atLocation location: ARHitTestResult){
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
+        if let sceneNode = scene.rootNode.childNode(withName: "ship",recursively:true) {
+            sceneNode.position = SCNVector3(
+                x: location.worldTransform.columns.3.x,
+                y: location.worldTransform.columns.3.y,
+                z: location.worldTransform.columns.3.z
+            )
+            
+            sceneNode.runAction(SCNAction.fadeOpacity(to: 0, duration: 10))
+            
+            planeArray.append(sceneNode)
+            
+            sceneView.scene.rootNode.addChildNode(sceneNode)
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    @IBAction func removePlane(_ sender: Any) {
+        if !planeArray.isEmpty{
+            for plane in planeArray{
+                plane.removeFromParentNode()
+            }
+        }
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    //MARK: - ARSCNViewDelegateMethods
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        
+//        if anchor is ARPlaneAnchor {
+//            let planeAnchor = anchor as! ARPlaneAnchor
+        
+        let planeNode = createPlane(withPlaneAnchor: planeAnchor)
+        
+            node.addChildNode(planeNode)
+            
+//        }else{
+//            return
+//        }
     }
+    
+    //MARK: - Plane Rendering Methods
+    
+    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode{
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let gridMaterial = SCNMaterial()
+        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+        plane.materials = [gridMaterial]
+        let planeNode = SCNNode()
+        planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        
+        planeNode.geometry = plane
+        
+        return planeNode
+    }
+    
+    
 }
